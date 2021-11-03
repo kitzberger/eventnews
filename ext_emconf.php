cea0c6c588d044dbc4a8aa4be079ed28bea4df5c
Philipp Kitzberger <coding@kitze.net>
2021-11-03 09:10:44 +0100

    Raise dependency to EXT:news version 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.13-10.4.99',
-            'news' => '7.0.0-8.9.99',
+            'news' => '7.0.0-9.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
